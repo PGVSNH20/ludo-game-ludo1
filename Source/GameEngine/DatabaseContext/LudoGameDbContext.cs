ab09a39abd0142c601107b9e19f8a5a5eaa8a8ea
Edgar Runnman <edgar.runnman@gmail.com>
2021-03-31 16:03:34 +0200

    DbContext

---
+++ Source/GameEngine/DatabaseContext/LudoGameDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GameEngine.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine.DatabaseContext
+{
+    internal class LudoGameDbContext : DbContext
+    {
+        public DbSet<GamePlayer> Players { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=LudoGameDb;User Id=sa;Password=My!P@ssw0rd1;");
+        }
+    }
+}

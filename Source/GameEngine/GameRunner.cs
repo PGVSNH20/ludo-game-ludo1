92bc6a8277b930a00477b931139859273b9dc8e6
Edgar Runnman <edgar.runnman@gmail.com>
2021-04-01 11:07:05 +0200

    working on GameRunner and LudoGame classes

--- Source/GameEngine/GameRunner.cs
+++ Source/GameEngine/GameRunner.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using GameEngine.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GameEngine
 {
-    class GameRunner
+    public class GameRunner
     {
+        public LudoGame CurrentGame { get; set; }
+        public GameDice Dice { get; set; }
+        public GameBoard Board { get; set; }
+
+        public GameRunner()
+        {
+            Dice = new GameDice();
+            Board = new GameBoard();
+        }
+
+        public void CreateNewGame()
+        {
+            CurrentGame = new LudoGame();
+        }
+
+        public void LoadGame()
+        {
+            //get game from db
+        }
     }
 }
--- Source/GameEngine/LudoGame.cs
+++ Source/GameEngine/LudoGame.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using GameEngine.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GameEngine
 {
-    class LudoGame
+    public class LudoGame
     {
+        public int LudoGameId { get; set; }
+        public List<GamePlayer> Players { get; set; }
+        public List<GameMove> Moves { get; set; }
+        public GamePlayer Winner { get; set; }
+        public string Status { get; set; }
+        public List<GamePiece> PieceSetup { get; set; }
+        public GamePlayer NextTurnPlayer { get; set; }
     }
 }

d1e065fc20b96fe64392812970a2e0f42aa2d489
Edgar Runnman <edgar.runnman@gmail.com>
2021-04-07 15:18:45 +0200

    påbörjade AI spelare

---
+++ Source/GameEngine/Assets/AIPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine.Assets
+{
+    internal class AIPlayer
+    {
+        //simulering av spelare
+    }
+}
